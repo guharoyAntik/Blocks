7ca5ea7cfdb496b36ff6593b5081ed9da4f59252
Antik <guharoyantik@gmail.com>
2022-07-15 08:31:45 +0530

    Started integrating interstitial ads

--- Assets/Scripts/MainMenuScene/Managers/AdmobsManager.cs
+++ Assets/Scripts/MainMenuScene/Managers/AdmobsManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GoogleMobileAds.Api;
+using System;
+
 public class AdmobsManager : MonoBehaviour
 {
     public static AdmobsManager Instance;
@@ -22,4 +24,51 @@ public class AdmobsManager : MonoBehaviour
     {
         MobileAds.Initialize(initStatus => { });
     }
+
+    #region Interstitial ads
+
+    private InterstitialAd interstitial;
+
+    private void RequestInterstitial()
+    {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(adUnitId);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+    }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                            + args.ToString());
+    }
+
+    public void HandleOnAdOpening(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpening event received");
+        // Pause game sounds
+    }
+
+    public void HandleOnAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+        interstitial.Destroy();
+        //Resume game sounds
+    }
+    #endregion
 }
